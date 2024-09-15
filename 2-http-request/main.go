2fe54add2ca26562e9d01fe5e1a87e67cd22420d
vitor.lima <vitorg.34825@gmail.com>
2024-09-15 13:42:48 +0000

    feat:

---
+++ 2-http-request/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		panic(err)
+	}
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(data))
+	resp.Body.Close()
+}
