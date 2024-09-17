e5668253adc278a71e9ff2dac1c8a5dc4d18d9a2
vitor.lima <vitorg.34825@gmail.com>
2024-09-17 14:26:44 +0000

    feat: :sparkles: db

--- 6-modulo-banco-de-dados/db.sql
+++ 6-modulo-banco-de-dados/db.sql
@@ -1,9 +1,3 @@
--- type Product struct {
--- 	ProductId uuid.UUID
--- 	Name      string
--- 	Price     float64
--- }
-
 create table products (
     product_id uuid primary key default gen_random_uuid(),
     name text,
